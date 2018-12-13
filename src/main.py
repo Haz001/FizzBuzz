752cc6b3e1b2cfd57b1e8055d9ec1ee42d9ff350
Haz <24993938+Haz001@users.noreply.github.com>
2018-12-13 01:03:07 +0000

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,23 @@
+c1 = "Fizz"
+c2 = "Buzz"
+r1 = 3
+r2 = 5
+cr = ""
+
+count = 0
+try:
+    limit = int(input("Limit:\n>"))
+except:
+    limit = 100
+
+while (count <= limit):
+    count += 1
+    cr = ""
+    if (count%r1)==0:
+        cr += c1
+    if (count%r2)==0:
+        cr += c2
+    if(cr == ""):
+        cr = str(count)
+    print (cr)
+input()
